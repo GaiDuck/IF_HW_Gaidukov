3ceb6b69555377ee53d515bfd4f5f5a43b424fb1
Gaidukov Alexey <alex.gaidukov@yandex.ru>
2025-05-26 22:53:36 +0300

    CUCUMBER не работает

--- src/test/java/org/ifellow/gaidukov/IF_HW4/CucumberRunnerTest.java
+++ src/test/java/org/ifellow/gaidukov/IF_HW4/CucumberRunnerTest.java
@@ -2,17 +2,18 @@ package org.ifellow.gaidukov.IF_HW4;
 
 import org.junit.platform.suite.api.ConfigurationParameter;
 import org.junit.platform.suite.api.IncludeEngines;
+import org.junit.platform.suite.api.SelectClasspathResource;
 import org.junit.platform.suite.api.Suite;
 
 import static io.cucumber.junit.platform.engine.Constants.*;
 
 @Suite
 @IncludeEngines("cucumber")
-//@SelectClasspathResource("src/main/java/org/ifellow/gaidukov/IF_HW4/steps/jiraSteps")
+@SelectClasspathResource("src/main/java/org/ifellow/gaidukov/IF_HW4/steps/jiraSteps")
 @ConfigurationParameter(key = FEATURES_PROPERTY_NAME, value = "srс/test/resources/feature")
 @ConfigurationParameter(key = PLUGIN_PROPERTY_NAME, value = "pretty")
 @ConfigurationParameter(key = EXECUTION_DRY_RUN_PROPERTY_NAME, value = "false")
-@ConfigurationParameter(key = GLUE_PROPERTY_NAME, value = "src/main/java/org/ifellow/gaidukov/IF_HW4/steps/jiraSteps, src/test/java/org/ifellow/gaidukov/IF_HW4/hook/CucumberRunnerTest")
+//@ConfigurationParameter(key = GLUE_PROPERTY_NAME, value = "src/main/java/org/ifellow/gaidukov/IF_HW4/steps/jiraSteps, src/test/java/org/ifellow/gaidukov/IF_HW4")
 
 public class CucumberRunnerTest {
 }
